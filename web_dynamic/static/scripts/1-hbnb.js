e704cee1bdf87709ca1e07522ea52ec2b1fe4e72
Kimberly Wong <kjowong@gmail.com>
2017-09-28 23:03:24 +0000

    Removing unnecessary variable names

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -6,13 +6,13 @@ $(document).ready(function () {
     let id = $(this).attr('data-id');
     let name = $(this).attr('data-name');
     if ($(this).is(':checked')) {
-      let idList = amenityIds.push(id);
-      let nameList = amenityNames.push(name);
-      $('DIV.amenities h4').text(amenityNames.sort().join(', '));
+      amenityIds.push(id);
+      amenityNames.push(name);
+      $('div.amenities h4').text(amenityNames.sort().join(', '));
     } else {
       amenityIds.splice($.inArray(id, amenityIds), 1);
       amenityNames.splice($.inArray(name, amenityNames), 1);
-      $('DIV.amenities h4').text(amenityNames.sort().join(', '));
+      $('div.amenities h4').text(amenityNames.sort().join(', '));
     }
   });
 });
