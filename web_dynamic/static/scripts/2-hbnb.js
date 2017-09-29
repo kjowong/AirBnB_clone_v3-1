ed36323cade3ed97245c55d4fef93eb6e1f392a7
Kimberly Wong <kjowong@gmail.com>
2017-09-29 00:19:46 +0000

    Adding new route to api

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,21 @@
+// Request API http://0.0.0.0:5001/api/v1/status/
+$(document).ready(function () {
+  let amenityIds = [];
+  let amenityNames = [];
+  $('input').bind('click', function () {
+    let id = $(this).attr('data-id');
+    let name = $(this).attr('data-name');
+    if ($(this).is(':checked')) {
+      amenityIds.push(id);
+      amenityNames.push(name);
+      $('div.amenities h4').text(amenityNames.sort().join(', '));
+    } else {
+      amenityIds.splice($.inArray(id, amenityIds), 1);
+      amenityNames.splice($.inArray(name, amenityNames), 1);
+      $('div.amenities h4').text(amenityNames.sort().join(', '));
+    }
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+	  console.log('Status', textStatus);
+  });
+});
