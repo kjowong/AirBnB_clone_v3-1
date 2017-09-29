4fc179165b1c281a4042b488864002d93cbbd8ee
Carrie Ybay <carrieybay@gmail.com>
2017-09-29 13:38:15 -0700

    Add console.log test statements

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,16 @@
 // amenities input
 $(document).ready(function () {
   let amenity_ids = [];
+  console.log("Amenity_ids: " + amenity_ids);
   $('input').change(function () {
     if (this.checked) {
       amenity_ids.push(:amenity_id);
       $('DIV.amenities.h4').append(:amenity_name);
+      console.log("Push / Append: " + amenity_ids);
     } else {
       amenity_ids.splice($.inArray(:amenity_id, amenity_ids), 1);
       $('DIV.amenities.h4').detach(:amenity_name);
+      console.log("Remove / Detach " + amenity_ids);
     }
   });
 });
