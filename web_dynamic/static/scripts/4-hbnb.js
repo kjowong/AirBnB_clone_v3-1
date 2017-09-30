ed5b2bf8707276d41bfd6be555090f17dabdb162
Kimberly Wong <kjowong@gmail.com>
2017-09-30 06:46:57 +0000

    Adding second ajax call

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,13 +1,5 @@
 // Connect Place and Amenity
 $(document).ready(function () {
- // function to help sort names properly
-  function compare (a, b) {
-  	let nameA = a.name.toUpperCase();
-    	let nameB = b.name.toUpperCase();
-   	if (nameA < nameB) { return -1; }
-    	if (nameA > nameB) { return 1; }
-    	return 0;
-  }
   // Creating amenity object with id and name
   let amenityObj = {};
   $('input').bind('click', function () {
@@ -45,13 +37,46 @@ $(document).ready(function () {
     contentType: 'application/json; charset=utf-8',
     dataType: 'JSON'
   }).done(function (data) {
-	  // console.log(data);
+ // function to help sort names properly
+    function compare (a, b) {
+  	let nameA = a.name.toUpperCase();
+    	let nameB = b.name.toUpperCase();
+   	if (nameA < nameB) { return -1; }
+    	if (nameA > nameB) { return 1; }
+    	return 0;
+    }
     data.sort(compare);
-    console.log(data);
     for (let i = 0; i < data.length; i++) {
       let place = data[i];
-      let placeHtml = '<article><div class="title"><h2>' + place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + place.number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + place.number_bathrooms + ' Bathroom</div></div><div class="description">' + place.description + '</div></article>';
+      let placeHtml = '<article><div class="title"><h2>' + place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + place.number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + place.number_bathrooms + ' Bathroom</div></div><div class="description"><br />' + place.description + '</div></article>';
       $('section.places').append(placeHtml);
     }
+  });
+
+// BUTTON CLICK
+  $('button').on('click', function () {
+		  console.log(amenityObj);
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      data: JSON.stringify(amenityObj),
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'JSON'
+    }).done(function (data) {
+ // function to help sort names properly
+      function compare (a, b) {
+  	let nameA = a.name.toUpperCase();
+    	let nameB = b.name.toUpperCase();
+   	if (nameA < nameB) { return -1; }
+    	if (nameA > nameB) { return 1; }
+    	return 0;
+      }
+      data.sort(compare);
+      for (let i = 0; i < data.length; i++) {
+        let place = data[i];
+        let placeHtml = '<article><div class="title"><h2>' + place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + place.number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + place.number_bathrooms + ' Bathroom</div></div><div class="description"><br />' + place.description + '</div></article>';
+        $('section.places').append(placeHtml);
+      }
+    });
   });
 });
