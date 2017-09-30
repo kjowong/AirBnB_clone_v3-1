2affaae7972893bde8019c350dd9e7566d9ee2ef
Carrie Ybay <carrieybay@gmail.com>
2017-09-30 12:26:12 -0700

    Change sort function for natural sort

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -38,14 +38,8 @@ $(document).ready(function () {
     dataType: 'JSON'
   }).done(function (data) {
  // function to help sort names properly
-    function compare (a, b) {
-      let nameA = a.name.toUpperCase();
-      let nameB = b.name.toUpperCase();
-      if (nameA < nameB) { return -1; }
-      if (nameA > nameB) { return 1; }
-      return 0;
-    }
-    data.sort(compare);
+    let collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
+    data.sort(collator.compare);
     for (let i = 0; i < data.length; i++) {
       let place = data[i];
       let placeHtml = '<article><div class="title"><h2>' + place.name + '</h2><div class="price_by_night">$' + place.price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + place.number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + place.number_bathrooms + ' Bathroom</div></div><div class="description"><br />' + place.description + '</div></article>';
