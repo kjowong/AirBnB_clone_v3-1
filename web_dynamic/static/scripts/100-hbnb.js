07a02eb2400f421f12a82c44b35ff49a9d29c51b
Kimberly Wong <kjowong@gmail.com>
2017-10-01 13:04:55 +0000

    Adding comments

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -1,10 +1,15 @@
-// Connect Place and Amenity
+// Only load when document is ready
 $(document).ready(function () {
   // Creates stateObj
   let stateObj = {};
+
+  // Binds a click event to the input tag for States
   $('div.locations ul.popover li h2 input').bind('click', function () {
+    // Grabs attribute value from the input tag
     let id = $(this).attr('data-id');
     let name = $(this).attr('data-name');
+
+    // If input is checked, store attributes in the state object
     if ($(this).is(':checked')) {
       stateObj[id] = name;
     } else {
@@ -15,9 +20,14 @@ $(document).ready(function () {
 
   // Creates cityObj
   let cityObj = {};
+
+  // Binds a click event to the input tag for City
   $('div.locations ul.popover li input').bind('click', function () {
+    // Grabs attribute value from the input tag
     let id = $(this).attr('data-id');
     let name = $(this).attr('data-name');
+
+    // If input is checked, store attributes to the city object
     if ($(this).is(':checked')) {
       cityObj[id] = name;
     } else {
@@ -33,10 +43,10 @@ $(document).ready(function () {
       return value;
     }).sort().join(', ');
 
-    // clear the div
+    // Clear the div
     $('div.locations h4').val('');
 
-    // replaces div with new array
+    // Replaces div with new array
     if (newLocationArray.length > 0) {
       $('div.locations h4').text(newLocationArray);
     } else {
@@ -44,26 +54,31 @@ $(document).ready(function () {
     }
   }
 
-  // Creating amenity object with id and name
+  // Creating amenity object
   let amenityObj = {};
+
+  // Binds a click event to the input tag for Amenity
   $('div.amenities ul.popover li input').bind('click', function () {
+    // Grabs attribute value from the input tag
     let id = $(this).attr('data-id');
     let name = $(this).attr('data-name');
+
+    // If input is checked, store attributes in the amenity object
     if ($(this).is(':checked')) {
       amenityObj[id] = name;
     } else {
       delete amenityObj[id];
     }
 
-    // Creates new array with values of amenityObj
+    // Creates new array from amenity object
     let newAmenityArray = $.map(amenityObj, function (value) {
       return value;
     }).sort().join(', ');
 
-    // clear the div
+    // Clear the div
     $('div.amenities h4').val(' ');
 
-    // replaces div with new array
+    // Replaces div with new array
     if (newAmenityArray.length > 0) {
       $('div.amenities h4').text(newAmenityArray);
     } else {
@@ -80,14 +95,14 @@ $(document).ready(function () {
     }
   });
 
-  // helper compare function to natural sort names with alphanumerical and lower/uppercase
+  // Helper compare function to natural sort names with alphanumerical and lower/uppercase
   function nameSort (a, b) {
     let name1 = a.name.toUpperCase();
     let name2 = b.name.toUpperCase();
     return name1.localeCompare(name2, undefined, { numeric: true, sensitivity: 'base' });
   }
 
-  // self invoking .get() for Users per Place
+  // Self invoking .get() for Users per Place
   let usersPerPlaceObj = {};
   (function getUsersPlace () {
     $.get('http://0.0.0.0:5001/api/v1/users/', {}).done(function (data) {
@@ -97,7 +112,7 @@ $(document).ready(function () {
     });
   }());
 
-  // ajax call function for Places
+  // Ajax call function for Places
   function ajaxCall (url, params = {}) {
     $.ajax({
       type: 'POST',
@@ -107,47 +122,76 @@ $(document).ready(function () {
       dataType: 'JSON'
     }).done(function (data) {
       data.sort(nameSort);
+
+      // Empties out the place's section
       $('section.places').empty();
+
+      // Goes through places data, appending places attributes/tags to the article
       for (let i = 0; i < data.length; i++) {
         let place = data[i];
+
+	// Creates the article tags
         let article = $('<article>');
-        // variable with place's main info
+
+        // Variable with place's main info
         let placeInfo = $('<div>').append($('<div>', {class: 'price_by_night', text: '$' + place.price_by_night})).append($('<div>', {class: 'title'})).append($('<h2>', {text: place.name}));
-        // variable with place's icon information
+
+        // Variable with place's icon information
         let placeIconInfo = '<div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + place.number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + place.number_bathrooms + ' Bathroom</div></div>';
+
+	// Appends all the place's icon's info to main place tag
         placeInfo.append(placeIconInfo);
-        // variable with place's owner and description
+
+        // If there description is null, set to empty string so null doesn't append
         if (!place.description) { place.description = ''; }
+
+	// Variable with place's owner and description, appending to main place tag
         let placeOwnDescription = placeInfo.append($('<div>', {class: 'user'})).append($('<strong>', {text: 'Owner: ' + usersPerPlaceObj[place.user_id]})).append('<br />').append($('div', {class: 'description'})).append('<br />' + place.description);
-        // append place to the article, then append article to places section
+
+	// Append place to the article, then append article to places section
         placeInfo.append(placeOwnDescription);
 
+        // Creates Amenities object for each place
         let amenitiesPlace = {};
+
+	// Get request to grab all amenities for each place
         $.get('http://0.0.0.0:5001/api/v1/places/' + place.id + '/amenities', {}).done(function (data) {
           for (let i = 0; i < data.length; i++) {
             amenitiesPlace[data[i].id] = data[i].name;
           }
+
+          // Creates a new array with just the values of the amenities obj
           let amenityPerPlaceArray = $.map(amenitiesPlace, function (value) {
             return (value);
           });
+
+	  // Variable with the amenities' information
           let amenitiesInfo = $('<div>', {class: 'amenities'}).append($('<h2>', {text: 'Amenities'}));
+
+	  // Creates a ul tag
           let ulTag = $('<ul>');
+
+	  // Goes over each value in the amenities array created before, appends the value in an li tag to the ul tag
           $.each(amenityPerPlaceArray, function (index, value) {
             ulTag.append($('<li>', {text: value}));
           });
+
+	  // Appends the ul tag with list of amenities to amnenities information then to the main place tag - appends place to article tag
           amenitiesInfo.append(ulTag);
           placeInfo.append(amenitiesInfo);
           article.append(placeInfo);
         });
+
+        // Appends complete article tag to the place's section
         $('section.places').append(article);
       }
     });
   }
 
-// ajax request for all Places
+// Ajax request for all Places
   ajaxCall('http://0.0.0.0:5001/api/v1/places_search/');
 
-// ajax request for Places with selected Amenities, States, and Cities
+// Ajax request for Places with selected Amenities, States, and Cities
   $('button').on('click', function () {
     ajaxCall('http://0.0.0.0:5001/api/v1/places_search/', {'amenities': amenityObj, 'states': stateObj, 'cities': cityObj});
   });
