7899f2b0e09eeaae3d0d45b12c7f19f3cb7f1660
Kimberly Wong <kjowong@gmail.com>
2017-10-01 21:48:39 +0000

    Fixed append

--- web_dynamic/static/scripts/101-hbnb.js
+++ web_dynamic/static/scripts/101-hbnb.js
@@ -1,4 +1,4 @@
-const failed = '<h1 class="nope">No places found....</h1><img src="../static/images/rage-horror.svg" id="fail-img"><h1 class="nope">Maybe you just fail at life</h1>';
+const failed = '<h1 class="nope">No places found....<img src="../static/images/rage-horror.svg" id="fail-img"></h1><h1 class="nope">Maybe you just fail at life</h1>';
 
 // Only load when document is ready
 $(document).ready(function () {
@@ -121,148 +121,149 @@ $(document).ready(function () {
       contentType: 'application/json; charset=utf-8',
       dataType: 'JSON'
     }).done(function (data) {
-      if (data.length === 0) { $(failed).appendTo('main'); }
       data.sort(nameSort);
 
       // Empties out the place's section
       $('section.places').empty();
 
+      if (data.length > 0) {
       // Goes through places data, appending places attributes/tags to the article
-      for (let i = 0; i < data.length; i++) {
-        let place = data[i];
+        for (let i = 0; i < data.length; i++) {
+          let place = data[i];
 
         // Creates the article tags
-        let article = $('<article>');
+          let article = $('<article>');
 
         // Variable with place's main info
-        let placeInfo = $('<div>').append($('<div>', {class: 'price_by_night', text: '$' + place.price_by_night})).append($('<div>', {class: 'title'})).append($('<h2>', {text: place.name}));
+          let placeInfo = $('<div>').append($('<div>', {class: 'price_by_night', text: '$' + place.price_by_night})).append($('<div>', {class: 'title'})).append($('<h2>', {text: place.name}));
 
         // Variable with place's icon information
-        let placeIconInfo = '<div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + place.number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + place.number_bathrooms + ' Bathroom</div></div>';
+          let placeIconInfo = '<div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + place.number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + place.number_bathrooms + ' Bathroom</div></div>';
 
         // Appends all the place's icon's info to main place tag
-        placeInfo.append(placeIconInfo);
+          placeInfo.append(placeIconInfo);
 
         // If there description is null, set to empty string so null doesn't append
-        if (!place.description) { place.description = ''; }
+          if (!place.description) { place.description = ''; }
 
         // Variable with the place's description, appending to main place tag
-        let placeOwnDescription = placeInfo.append($('<div>', {class: 'user'})).append($('<strong>', {text: 'Owner: ' + usersPerPlaceObj[place.user_id]})).append('<br />').append($('div', {class: 'description'})).append('<br />' + place.description);
+          let placeOwnDescription = placeInfo.append($('<div>', {class: 'user'})).append($('<strong>', {text: 'Owner: ' + usersPerPlaceObj[place.user_id]})).append('<br />').append($('div', {class: 'description'})).append('<br />' + place.description);
 
         // Append place to the article, then append article to places section
-        placeInfo.append(placeOwnDescription);
+          placeInfo.append(placeOwnDescription);
 
         // Creates Amenities object for each place
-        let amenitiesPlace = {};
+          let amenitiesPlace = {};
 
         // Get request to grab all amenities for each place
-        $.get('http://0.0.0.0:5001/api/v1/places/' + place.id + '/amenities', {}).done(function (data) {
-          for (let i = 0; i < data.length; i++) {
-            amenitiesPlace[data[i].id] = data[i].name;
-          }
+          $.get('http://0.0.0.0:5001/api/v1/places/' + place.id + '/amenities', {}).done(function (data) {
+            for (let i = 0; i < data.length; i++) {
+              amenitiesPlace[data[i].id] = data[i].name;
+            }
 
           // Creates a new array with just the values of the amenities obj
-          let amenityPerPlaceArray = $.map(amenitiesPlace, function (value) {
-            return (value);
-          });
+            let amenityPerPlaceArray = $.map(amenitiesPlace, function (value) {
+              return (value);
+            });
 
           // Variable with the amenities' information
-          let amenitiesInfo = $('<div>', {class: 'amenities'}).append($('<h2>', {text: 'Amenities'}));
+            let amenitiesInfo = $('<div>', {class: 'amenities'}).append($('<h2>', {text: 'Amenities'}));
 
           // Creates a ul tag
-          let ulTag = $('<ul>');
+            let ulTag = $('<ul>');
 
           // Goes over each value in the amenities array created before, appends the value in an li tag to the ul tag
-          $.each(amenityPerPlaceArray, function (index, value) {
-            ulTag.append($('<li>', {text: value}));
-          });
+            $.each(amenityPerPlaceArray, function (index, value) {
+              ulTag.append($('<li>', {text: value}));
+            });
 
           // Appends the ul tag with list of amenities to amnenities information - then appends all amenities to the main place tag
-          amenitiesInfo.append(ulTag);
-          placeInfo.append(amenitiesInfo);
-        });
+            amenitiesInfo.append(ulTag);
+            placeInfo.append(amenitiesInfo);
+          });
 
         // Creates reviews objects based on a place
-        let reviewsPlaceText = {};
-        let reviewsPlaceDate = {};
-        let reviewsPlaceUser = {};
+          let reviewsPlaceText = {};
+          let reviewsPlaceDate = {};
+          let reviewsPlaceUser = {};
 
         // Get request to grab all reviews for each place
-        $.get('http://0.0.0.0:5001/api/v1/places/' + place.id + '/reviews', {}).done(function (data) {
-          for (let i = 0; i < data.length; i++) {
-            reviewsPlaceText[data[i].user_id] = data[i].text;
+          $.get('http://0.0.0.0:5001/api/v1/places/' + place.id + '/reviews', {}).done(function (data) {
+            for (let i = 0; i < data.length; i++) {
+              reviewsPlaceText[data[i].user_id] = data[i].text;
 
           // Creates new user object with all users in one review
-            if (usersPerPlaceObj[data[i].user_id]) { reviewsPlaceUser[data[i].user_id] = usersPerPlaceObj[data[i].user_id]; }
+              if (usersPerPlaceObj[data[i].user_id]) { reviewsPlaceUser[data[i].user_id] = usersPerPlaceObj[data[i].user_id]; }
 
             // Converts UTC time to long format, grabbing only Month, Date and Year. Ex: Mar 25 2017
-            let dateString = new Date(data[i].created_at).toString().split(' ');
-            dateString = dateString[1] + ' ' + dateString[2] + ' ' + dateString[3];
+              let dateString = new Date(data[i].created_at).toString().split(' ');
+              dateString = dateString[1] + ' ' + dateString[2] + ' ' + dateString[3];
 
-            reviewsPlaceDate[data[i].user_id] = dateString;
-          }
+              reviewsPlaceDate[data[i].user_id] = dateString;
+            }
 
           //
-          let reviewsCount = data.length;
+            let reviewsCount = data.length;
 
           // Variable with main review's information, append to div
-          let reviewsInfo = $('<div>', {class: 'reviews'}).append($('<h2>', {text: reviewsCount + ' Reviews'}).append($('<span>', {text: 'Show'})));
+            let reviewsInfo = $('<div>', {class: 'reviews'}).append($('<h2>', {text: reviewsCount + ' Reviews'}).append($('<span>', {text: 'Show'})));
 
           // Creates the ul tag with class toggle_reviews - allows user to toggle the reviews (show/hide)
-          let ulTag = $('<ul>', {class: 'toggle_reviews'});
+            let ulTag = $('<ul>', {class: 'toggle_reviews'});
 
           // Initialize variable to put together each review
-          let reviewer = '';
-          let reviewDate = '';
-          let review = '';
+            let reviewer = '';
+            let reviewDate = '';
+            let review = '';
 
           // Array to store each review object
-          let reviewArr = [];
+            let reviewArr = [];
 
           // Traverse over all the users in each review
-          for (let key in reviewsPlaceUser) {
+            for (let key in reviewsPlaceUser) {
             // If the key matches, then create the object
-            if (reviewsPlaceUser.hasOwnProperty(key) === reviewsPlaceDate.hasOwnProperty(key)) {
-              reviewer = reviewsPlaceUser[key];
-              reviewDate = reviewsPlaceDate[key];
-              review = reviewsPlaceText[key];
-            }
+              if (reviewsPlaceUser.hasOwnProperty(key) === reviewsPlaceDate.hasOwnProperty(key)) {
+                reviewer = reviewsPlaceUser[key];
+                reviewDate = reviewsPlaceDate[key];
+                review = reviewsPlaceText[key];
+              }
             // Push each review object to the review array
-            reviewArr.push({'name': reviewer, 'text': review, 'date': reviewDate});
-          }
+              reviewArr.push({'name': reviewer, 'text': review, 'date': reviewDate});
+            }
 
           // Checks if place has at least one review
-          if (reviewArr.length > 0) {
+            if (reviewArr.length > 0) {
             // Traverse over the review array
-            for (let i = 0; i < reviewArr.length; i++) {
+              for (let i = 0; i < reviewArr.length; i++) {
               // Creates the user's info
-              let userInfo = $('<h3>', {text: 'From ' + reviewArr[i].name + ' on ' + reviewArr[i].date});
+                let userInfo = $('<h3>', {text: 'From ' + reviewArr[i].name + ' on ' + reviewArr[i].date});
 
               // Creates a wrap to wrap the reviewer's info in a li tag
-              let userWrap = $('<li>');
-              userWrap.append(userInfo);
-              ulTag.append(userWrap);
+                let userWrap = $('<li>');
+                userWrap.append(userInfo);
+                ulTag.append(userWrap);
 
               // Converts text in reviewsPlace to html
-              let reviewsText = $('<p>').html(reviewArr[i].text);
+                let reviewsText = $('<p>').html(reviewArr[i].text);
 
               // Variable with the review's text, append to the ul tag
-              let userReview = $(reviewsText);
-              ulTag.append(userReview);
+                let userReview = $(reviewsText);
+                ulTag.append(userReview);
+              }
             }
-          }
 
           // Append complete ul tag to reviews - then append to main place tag
-          reviewsInfo.append(ulTag);
-          placeInfo.append(reviewsInfo);
-        });
+            reviewsInfo.append(ulTag);
+            placeInfo.append(reviewsInfo);
+          });
 
         // Append the complete place tag to the article
-        article.append(placeInfo);
+          article.append(placeInfo);
 
         // Append complete article tag to the place's section
-        $('section.places').append(article);
-      }
+          $('section.places').append(article);
+        }
+      } else { $(failed).appendTo($('.places')); }
     });
   }
 
