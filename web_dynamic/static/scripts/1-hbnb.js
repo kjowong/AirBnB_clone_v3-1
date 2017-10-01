24ea3f4913238e8b8e0abc80677d9bf8c8bbe7ec
Kimberly Wong <kjowong@gmail.com>
2017-10-01 12:35:40 +0000

    Adding comments

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,20 +1,30 @@
-// amenities input
+// Only load when document is ready
 $(document).ready(function () {
+  // Creates amenity object
   let amenityObj = {};
+
+  // Binds a click event to input tag
   $('input').bind('click', function () {
+    // Grabs attribute value from the input tag
     let id = $(this).attr('data-id');
     let name = $(this).attr('data-name');
+
+    // If the checkbox is checked, store attributes in the amenity object
     if ($(this).is(':checked')) {
       amenityObj[id] = name;
     } else {
       delete amenityObj[id];
     }
-    // clear the div
+
+    // Clear the div
     $('div.amenities h4').val('');
+
+    // Creates new array from amenity objects values
     let newAmenityArray = $.map(amenityObj, function (value) {
       return value;
     }).sort().join(', ');
-    // replaces div with new array
+
+    // Replaces div with new array
     $('div.amenities h4').text(newAmenityArray);
   });
 });
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,22 +1,34 @@
-// Request API http://0.0.0.0:5001/api/v1/status/
+// Only load when document is ready
 $(document).ready(function () {
+  // Creates amenity object
   let amenityObj = {};
+
+  // Binds a click event to input tag
   $('input').bind('click', function () {
+    // Grabs attribute value from the input tag
     let id = $(this).attr('data-id');
     let name = $(this).attr('data-name');
+
+    // If input is checked, store attributes in the amenity object
     if ($(this).is(':checked')) {
       amenityObj[id] = name;
     } else {
       delete amenityObj[id];
     }
-    // clear the div
+
+    // Clear the div
     $('div.amenities h4').val('');
+
+    // Creates new array from amenity objects values
     let newAmenityArray = $.map(amenityObj, function (value) {
       return value;
     }).join(', ');
-    // replaces div with new array
+
+    // Replaces div with new array
     $('div.amenities h4').text(newAmenityArray);
   });
+
+  // Request API http://0.0.0.0:5001/api/v1/status/
   $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
     if (textStatus === 'success') {
       $('div#api_status').addClass('available');
