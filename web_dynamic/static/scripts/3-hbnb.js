230b6a8f3195ad25c099ace749b313086febcff7
Kimberly Wong <kjowong@gmail.com>
2017-10-01 12:46:11 +0000

    Adding comments

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,22 +1,33 @@
-// Append Place to Article
+// Only load when document is ready
 $(document).ready(function () {
+  // Creates amenity object
   let amenityObj = {};
+
+  // Binds a click event to input tag
   $('input').bind('click', function () {
+    // Grabs attribute value from the input tag
     let id = $(this).attr('data-id');
     let name = $(this).attr('data-name');
+
+    // If input is checked, store attributes in the amenity object
     if ($(this).is(':checked')) {
       amenityObj[id] = name;
     } else {
       delete amenityObj[id];
     }
-    // clear the div
+
+    // Clear the div
     $('div.amenities h4').val('');
+
+    // Creates new array from amenity object values
     let newAmenityArray = $.map(amenityObj, function (value) {
       return value;
     }).join(', ');
-    // replaces div with new array
+
+    // Replaces div with new array
     $('div.amenities h4').text(newAmenityArray);
   });
+
   // Request API http://0.0.0.0:5001/api/v1/status/
   $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
     if (textStatus === 'success') {
@@ -25,12 +36,14 @@ $(document).ready(function () {
       $('div#api_status').removeClass('available');
     }
   });
-  // function to sort names of place properly
+
+  // Helper compare function to natural sort names with alphanumerical and lower/uppercase
   function nameSort (a, b) {
     let name1 = a.name.toUpperCase();
     let name2 = b.name.toUpperCase();
     return name1.localeCompare(name2, undefined, { numeric: true, sensitivity: 'base' });
   }
+
   // Post Places http://0.0.0.0:5001/api/v1/places_search/
   $.ajax({
     type: 'POST',
