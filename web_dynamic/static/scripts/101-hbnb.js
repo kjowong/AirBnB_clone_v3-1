490ba8120c4f24898236559e630baaa7a7a2188f
Kimberly Wong <kjowong@gmail.com>
2017-10-01 11:41:07 +0000

    Adding comments

--- web_dynamic/static/scripts/101-hbnb.js
+++ web_dynamic/static/scripts/101-hbnb.js
@@ -33,10 +33,10 @@ $(document).ready(function () {
       return value;
     }).sort().join(', ');
 
-    // clear the div
+    // Clear the div
     $('div.locations h4').val('');
 
-    // replaces div with new array
+    // Replaces div with new array
     if (newLocationArray.length > 0) {
       $('div.locations h4').text(newLocationArray);
     } else {
@@ -60,10 +60,10 @@ $(document).ready(function () {
       return value;
     }).sort().join(', ');
 
-    // clear the div
+    // Clear the div
     $('div.amenities h4').val(' ');
 
-    // replaces div with new array
+    // Replaces div with new array
     if (newAmenityArray.length > 0) {
       $('div.amenities h4').text(newAmenityArray);
     } else {
@@ -80,14 +80,14 @@ $(document).ready(function () {
     }
   });
 
-  // helper compare function to natural sort names with alphanumerical and lower/uppercase
+  // Helper compare function to natural sort names with alphanumerical and lower/uppercase
   function nameSort (a, b) {
     let name1 = a.name.toUpperCase();
     let name2 = b.name.toUpperCase();
     return name1.localeCompare(name2, undefined, { numeric: true, sensitivity: 'base' });
   }
 
-  // self invoking .get() for Users per Place
+  // Self invoking .get() for Users per Place
   let usersPerPlaceObj = {};
   (function getUsersPlace () {
     $.get('http://0.0.0.0:5001/api/v1/users/', {}).done(function (data) {
@@ -97,7 +97,7 @@ $(document).ready(function () {
     });
   }());
 
-  // ajax call function for Places
+  // Ajax call function for Places
   function ajaxCall (url, params = {}) {
     $.ajax({
       type: 'POST',
@@ -107,75 +107,125 @@ $(document).ready(function () {
       dataType: 'JSON'
     }).done(function (data) {
       data.sort(nameSort);
+
+      // Empties out the place's section
       $('section.places').empty();
+
+      // Goes through places data, appending places attributes/tags to the article
       for (let i = 0; i < data.length; i++) {
         let place = data[i];
+
+        // Creates the article tags
         let article = $('<article>');
-        // variable with place's main info
+
+        // Variable with place's main info
         let placeInfo = $('<div>').append($('<div>', {class: 'price_by_night', text: '$' + place.price_by_night})).append($('<div>', {class: 'title'})).append($('<h2>', {text: place.name}));
-        // variable with place's icon information
+
+        // Variable with place's icon information
         let placeIconInfo = '<div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + place.number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + place.number_bathrooms + ' Bathroom</div></div>';
+
+        // Appends all the place's icon's info to main place tag
         placeInfo.append(placeIconInfo);
-        // variable with place's owner and description
+
+        // If there description is null, set to empty string so null doesn't append
         if (!place.description) { place.description = ''; }
+
+        // Variable with the place's description, appending to main place tag
         let placeOwnDescription = placeInfo.append($('<div>', {class: 'user'})).append($('<strong>', {text: 'Owner: ' + usersPerPlaceObj[place.user_id]})).append('<br />').append($('div', {class: 'description'})).append('<br />' + place.description);
-        // append place to the article, then append article to places section
+
+        // Append place to the article, then append article to places section
         placeInfo.append(placeOwnDescription);
 
+        // Creates Amenities object for each place
         let amenitiesPlace = {};
+
+        // Get request to grab all amenities for each place
         $.get('http://0.0.0.0:5001/api/v1/places/' + place.id + '/amenities', {}).done(function (data) {
           for (let i = 0; i < data.length; i++) {
             amenitiesPlace[data[i].id] = data[i].name;
           }
+
+          // Creates a new array with just the values of the amenities obj
           let amenityPerPlaceArray = $.map(amenitiesPlace, function (value) {
             return (value);
           });
+
+          // Variable with the amenities' information
           let amenitiesInfo = $('<div>', {class: 'amenities'}).append($('<h2>', {text: 'Amenities'}));
+
+          // Creates a ul tag
           let ulTag = $('<ul>');
+
+          // Goes over each value in the amenities array created before, appends the value in an li tag to the ul tag
           $.each(amenityPerPlaceArray, function (index, value) {
             ulTag.append($('<li>', {text: value}));
           });
+
+          // Appends the ul tag with list of amenities to amnenities information - then appends all amenities to the main place tag
           amenitiesInfo.append(ulTag);
           placeInfo.append(amenitiesInfo);
         });
+
+        // Creates reviews object based on a place
         let reviewsPlace = {};
+
+        // Get request to grab all reviews for each place
         $.get('http://0.0.0.0:5001/api/v1/places/' + place.id + '/reviews', {}).done(function (data) {
           for (let i = 0; i < data.length; i++) {
             reviewsPlace['user_id'] = data[i].user_id;
             reviewsPlace['text'] = data[i].text;
+
+            // Converts UTC time to long format, grabbing only Month, Date and Year. Ex: Mar 25 2017
             let dateString = new Date(data[i].created_at).toString().split(' ');
             dateString = dateString[1] + ' ' + dateString[2] + ' ' + dateString[3];
+
             reviewsPlace['created_at'] = dateString;
           }
+
+          // Variable with main review's information, append to div
           let reviewsInfo = $('<div>', {class: 'reviews'}).append($('<h2>', {text: 'Reviews'}).append($('<span>', {text: 'Show'})));
+
+          // Creates the ul tag with class toggle_reviews - allows user to toggle the reviews (show/hide)
           let ulTag = $('<ul>', {class: 'toggle_reviews'});
+
+          // Checks if place has a review from a user, only grab the reviewer's info if the result is true
           if (usersPerPlaceObj[reviewsPlace.user_id]) {
             let userInfo = $('<h3>', {text: 'From ' + usersPerPlaceObj[reviewsPlace.user_id] + ' on ' + reviewsPlace.created_at});
+
+            // Creates a wrap to wrap the reviewer's info in a li tag
             let userWrap = $('<li>');
             userWrap.append(userInfo);
             ulTag.append(userWrap);
+
+            // Variable with the review's text, append to the ul tag
             let userReview = $('<p>', {text: reviewsPlace.text});
             ulTag.append(userReview);
           }
+
+          // Append complete ul tag to reviews - then append to main place tag
           reviewsInfo.append(ulTag);
           placeInfo.append(reviewsInfo);
         });
+
+        // Append the complete place tag to the article
         article.append(placeInfo);
+
+        // Append complete article tag to the place's section
         $('section.places').append(article);
       }
     });
   }
 
-  // show / hide Reviews section
+  // Show / Hide Reviews section
   $(document).on('click', 'div.reviews h2 span', function () {
     $(this).closest('div.reviews').find('ul.toggle_reviews').toggle();
     $(this).text($(this).text() === 'Show' ? 'Hide' : 'Show');
   });
 
-// ajax request for all Places
+// Ajax request for all Places
   ajaxCall('http://0.0.0.0:5001/api/v1/places_search/');
 
-// ajax request for Places with selected Amenities, States, and Cities
+// Ajax request for Places with selected Amenities, States, and Cities
   $('button').on('click', function () {
     ajaxCall('http://0.0.0.0:5001/api/v1/places_search/', {'amenities': amenityObj, 'states': stateObj, 'cities': cityObj});
   });
