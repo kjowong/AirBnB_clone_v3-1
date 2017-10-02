49fd024297a47a0cce33130af072f5986a8ecafa
Carrie Ybay <carrieybay@gmail.com>
2017-10-02 12:38:55 -0700

    Fix problem where menu moves when all amenities are unchecked

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -9,12 +9,18 @@ $(document).ready(function () {
     } else {
       delete amenityObj[id];
     }
-    // clear the div
-    $('div.amenities h4').val('');
     let newAmenityArray = $.map(amenityObj, function (value) {
       return value;
     }).sort().join(', ');
+
+    // clear the div
+    $('div.amenities h4').val('');
+
     // replaces div with new array
-    $('div.amenities h4').text(newAmenityArray);
+    if (newAmenityArray.length > 0) {
+      $('div.amenities h4').text(newAmenityArray);
+    } else {
+      $('div.amenities h4').text('\u00A0');
+    }
   });
 });
